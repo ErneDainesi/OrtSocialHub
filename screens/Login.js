c45839af6ea7e530714e5f1443b7770a52744e65
ErneDainesi <edainesi@fi.uba.ar>
2024-06-01 19:42:32 -0300

    add WIP for login

---
+++ screens/Login.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const Login = ({ navigation }) => {
+    return (
+        <View>
+            <Text>WIP</Text>
+        </View>
+    );
+}
+
+export default Login;
