44c4e2a7daa4f02c3b552191b4200d9a0ba8fba0
ErneDainesi <edainesi@fi.uba.ar>
2024-06-01 20:22:09 -0300

    remove styles from App

--- App.js
+++ App.js
@@ -25,12 +25,3 @@ export default function App() {
         </AuthProvider>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
