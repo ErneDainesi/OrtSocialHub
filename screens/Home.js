09703b65e8db7e0cd0919b311b18eebeaccac198
ErneDainesi <edainesi@fi.uba.ar>
2024-06-05 21:34:43 -0300

    add styles

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,25 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import Composer from "../components/Composer";
 import Feed from "../components/Feed";
 
 const Home = ({ navigation }) => {
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={styles.flex}>
             <Composer></Composer>
+            <Feed style={styles.feed}></Feed>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    flex: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    feed: {
+        marginTop: '2rem'
+    }
+});
 
 export default Home;
