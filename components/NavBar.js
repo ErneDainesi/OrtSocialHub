038089d2a42a55bf3896f366c916b982af11a084
unknown <matthewmccresteele123@gmail.com>
2024-06-11 22:46:24 -0300

    Basic structure of the NavBar Added

---
+++ components/NavBar.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { Button, Text, View , StyleSheet} from "react-native";
+
+import React from 'react'
+
+export const NavBar = () => {
+
+
+    return (
+    
+        <li>
+            <ul>
+            <Button></Button>
+            </ul>
+            <ul>
+            <Button></Button>
+            </ul>
+            <ul>
+            <Button></Button>
+            </ul>
+            <ul>
+            <Button></Button>
+            </ul>
+            
+        </li>
+  )}
+
+  
+
+//   const styles = StyleSheet.create({
+//     navBar:{
+//         margin:'auto',
+//         display:flex
+//     }
+//   }
+// )
+
