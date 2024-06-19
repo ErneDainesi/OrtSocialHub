9cbd176a0e41d9ac6afd67da7f8b04526bc9ef52
matthew <matthewmccreasteele123@gmail.com>
2024-06-18 21:26:14 -0300

    Icons Added

--- components/BottomNavBar.js
+++ components/BottomNavBar.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import Home from '../screens/Home';
 import Login from '../screens/Login';
 
@@ -7,11 +8,31 @@ const Tab = createBottomTabNavigator();
 
 const BottomNavBar = () => {
   return (
-        <Tab.Navigator>
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Login" component={Login} />
-        </Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Login') {
+            iconName = focused ? 'log-in' : 'log-in-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'grey',
+        inactiveTintColor: 'grey',
+        labelStyle: { paddingBottom: 10, fontSize: 15 },
+        style: { padding: 10, height: 70 },
+      }}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Login" component={Login} />
+    </Tab.Navigator>
   );
-}
+};
 
 export default BottomNavBar;
