b7a5139a9601193f5badf7017d307b4416a80f57
owendonn1 <owendonn@gmail.com>
2024-06-22 16:31:06 -0300

    Profile view update

--- screens/Profile.js
+++ screens/Profile.js
@@ -4,31 +4,59 @@ import { AuthContext } from "../context/AuthContext";
 import Feed from "../components/Feed";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Profile = ({ route }) => {
+const Profile = ({ route, navigation }) => {
     const [ownProfile, setOwnProfile] = useState(false);
-    const {profile, fetchUserProfile} = useContext(AuthContext)
+    const {profile, fetchUserProfile, followUser, unfollowUser, fetchFollowers, following} = useContext(AuthContext)
     const {userId} = route.params;
+    
     const isOwnProfile = async () => {
         const loggedInUserId = await AsyncStorage.getItem('loggedInUserId');
         setOwnProfile(loggedInUserId === userId);
     };
+    
     isOwnProfile();
     useEffect(() => {
         fetchUserProfile(userId);
     }, [userId]);
-    return (
+
+    const handleFollow = () => {
+        followUser(userId);
+    };
+
+    const handleUnfollow = () => {
+        unfollowUser(userId);
+    };
+
+    const handleViewFollowers= () =>{
+        navigation.navigate("Followers", {userId}); 
+    };
+
+    const isFollowing = following.includes(userId);
+
+    return(
         <View>
             {profile && (<>
-                <View>
-                    <Text>{`${profile.firstName} ${profile.lastName}`}</Text>
-                    <Button
-                        title={ownProfile ? "Edit" : "Follow/Unfollow"}
-                    ></Button>
-                </View>
-                <Feed id={profile.id} isProfile={true} />
+              <View>
+                 <Text>{`${profile.firstName} ${profile.lastName}`}</Text>
+                 {ownProfile ? (
+                    <Button title="Edit"/>
+                 ) :(
+                    <>
+                     {isFollowing ? (
+                        <Button title="Unfollow" onPress={handleUnfollow}/>
+                     ):(
+                        <Button title="Follow" onPress={handleFollow}/>
+                     )}
+                     <Button title="View Followers" onPress={handleViewFollowers} />
+                    </>
+                 )}
+              </View>
+              <Feed id={profile.id} isProfile={true} />
+            
             </>)}
         </View>
     );
+    
 }
 
 export default Profile;
