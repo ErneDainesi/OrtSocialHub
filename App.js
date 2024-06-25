4d63d2cddbe3125421887726bbfe97d9906210fa
ErneDainesi <edainesi@fi.uba.ar>
2024-06-24 23:33:29 -0300

    fix bottom bar

--- App.js
+++ App.js
@@ -1,50 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Register from './screens/Register';
-import Home from './screens/Home';
-import Error from './screens/Error';
-import Login from './screens/Login';
-import Profile from './screens/Profile';
 import { useContext } from 'react';
-import NewsFeed from './screens/NewsFeed';
 import { AuthContext, AuthProvider } from './context/AuthContext';
 import { FeedProvider } from './context/FeedContext'
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import BottomNavBar from './components/BottomNavBar';
-import BottomBar from './components/BottomNavBar';
+import { NotLoggedInBar, BottomNavBar } from './components/BottomNavBar';
 import { NewsPostProvider } from './context/NewsPostContext';
-
-const Stack = createStackNavigator();
-
-const AuthNavigator = () => {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
-            <Stack.Screen name="Register" component={Register} options={{headerShown: false}} />
-            <Stack.Screen name="Error" component={Error} />
-        </Stack.Navigator>
-    );
-}
-
-const AppNavigator = () => {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Register" component={Register} options={{headerShown: false}} />
-            <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
-            <Stack.Screen name="Error" component={Error} />
-            <Stack.Screen name="NewsFeed" component={NewsFeed} />  
-            <Stack.Screen name="Profile" component={Profile} />
-        </Stack.Navigator>
-    );
-}
 
 const Root = () => {
     const { isLoggedIn } = useContext(AuthContext);
     return (
         <FeedProvider>
             <NewsPostProvider>
-                {isLoggedIn ? <AppNavigator /> : <AuthNavigator />}
+                {isLoggedIn ? <BottomNavBar /> : <NotLoggedInBar />}
             </NewsPostProvider>
         </FeedProvider>
     );
--- components/BottomNavBar.js
+++ components/BottomNavBar.js
@@ -3,36 +3,63 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Home from '../screens/Home';
 import Login from '../screens/Login';
+import Register from '../screens/Register';
+import NewsFeed from '../screens/NewsFeed';
+import Profile from '../screens/Profile';
 
 const Tab = createBottomTabNavigator();
 
-const BottomNavBar = () => {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-
-          if (route.name === 'Home') {
-            iconName = focused ? 'home' : 'home-outline';
-          } else if (route.name === 'Login') {
-            iconName = focused ? 'log-in' : 'log-in-outline';
-          }
-
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-      })}
-      tabBarOptions={{
-        activeTintColor: 'grey',
-        inactiveTintColor: 'grey',
-        labelStyle: { paddingBottom: 10, fontSize: 15 },
-        style: { padding: 10, height: 70 },
-      }}
-    >
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Login" component={Login} />
-    </Tab.Navigator>
-  );
+export const BottomNavBar = () => {
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if (route.name === 'Home') {
+                        iconName = focused ? 'home' : 'home-outline';
+                    } else if (route.name === 'Login') {
+                        iconName = focused ? 'log-in' : 'log-in-outline';
+                    }
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+            })}
+            tabBarOptions={{
+                activeTintColor: 'grey',
+                inactiveTintColor: 'grey',
+                labelStyle: { paddingBottom: 10, fontSize: 15 },
+                style: { padding: 10, height: 70 },
+            }}
+        >
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="NewsFeed" component={NewsFeed} />  
+            <Tab.Screen name="Profile" component={Profile} />
+        </Tab.Navigator>
+    );
 };
 
-export default BottomNavBar;
+export const NotLoggedInBar = () => {
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+                    if (route.name === 'Home') {
+                        iconName = focused ? 'home' : 'home-outline';
+                    } else if (route.name === 'Login') {
+                        iconName = focused ? 'log-in' : 'log-in-outline';
+                    }
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+            })}
+            tabBarOptions={{
+                activeTintColor: 'grey',
+                inactiveTintColor: 'grey',
+                labelStyle: { paddingBottom: 10, fontSize: 15 },
+                style: { padding: 10, height: 70 },
+            }}
+        >
+            <Tab.Screen name="Login" component={Login} options={{tabBarShowLabel: false}} />
+            <Tab.Screen name="Register" component={Register} options={{tabBarShowLabel: false}} />
+        </Tab.Navigator>
+    );
+}
