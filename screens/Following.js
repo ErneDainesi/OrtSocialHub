2073c41d2eac060c8c16d816fdbcf825e3988cef
ErneDainesi <edainesi@fi.uba.ar>
2024-06-25 17:08:08 -0300

    add user item

--- screens/Following.js
+++ screens/Following.js
@@ -1,29 +1,35 @@
 import React from 'react'
 import { useContext, useEffect } from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, FlatList, StyleSheet } from "react-native";
 import { AuthContext } from "../context/AuthContext";
+import UserListItem from '../components/UserListItem';
 
 const Following = ({ route }) => {
-    const {userId} = route.params;
+    const { userId } = route.params;
     const {fetchFollowing, following} = useContext(AuthContext);
 
-    useEffect(() =>{
+    useEffect(() => {
         fetchFollowing(userId);
     }, [userId]);
 
     return (
-        <View>
+        <View style={styles.feed}>
             <FlatList
-              data={following}
-              keyExtractor={(item) => item.followerId}
-              renderItem={({item}) =>(
-                <View>
-                    <Text>{item.Followed.firstName} {item.Followed.lastName}</Text>
-                </View>    
-              )}
+                data={following}
+                keyExtractor={(item) => item.followerId}
+                renderItem={({item}) => <UserListItem user={item.Followed} />}
             />  
         </View>  
-  );
+    );
 };
+
+const styles = StyleSheet.create({
+	feed: {
+		marginTop: "2rem",
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+	}
+});
 
 export default Following;
